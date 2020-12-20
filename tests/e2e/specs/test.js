365c81a81d9a8e33b7cb265c41fd228d212183c7
Natan Kruger <natan.kruger@zygotecnologia.com>
2020-12-20 09:39:46 -0300

    test(e2e): simple e2e testing

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,26 @@
-// https://docs.cypress.io/api/introduction/api.html
-
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
+describe('App simple E2E test', () => {
+  it('Visits the Home page', () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
+    cy.contains('h1', 'Desafio de Frontend')
+  })
+
+  it('Visits the New Product page', () => {
+    cy.visit('/product/new')
+    cy.contains('h1', 'Novo produto')
+  })
+
+  it('Visits the Product list page', () => {
+    cy.visit('/products')
+    cy.contains('h1', 'Listagem de produtos')
+  })
+
+  it('Visits the Movement new page', () => {
+    cy.visit('/movement/new')
+    cy.contains('h1', 'Nova Movimentação')
+  })
+
+  it('Visits the Movement list page', () => {
+    cy.visit('/movements')
+    cy.contains('h1', 'Lucro por produto')
   })
 })
